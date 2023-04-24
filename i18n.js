b12693546081c1988217c213bc46fbe19b2243fa
TamNT <tamnt@hybrid-technologies.co.jp>
2023-04-24 08:47:06 +0700

    refactor: turn off log i18n

--- i18n.js
+++ i18n.js
@@ -10,4 +10,5 @@ module.exports = {
     // You can use a dynamic import, fetch, whatever. You should
     // return a Promise with the JSON file.
     import(`/src/locales/${lang}/${ns}.json`).then((m) => m.default),
+  logBuild: false,
 };
