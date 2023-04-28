83e9578a6e0c0d13029d287aa9709f12bc4a4bf0
TamNT <tamnt@hybrid-technologies.co.jp>
2023-04-28 10:23:33 +0700

    feat: update i18n config

--- i18n.js
+++ i18n.js
@@ -1,14 +1,14 @@
 module.exports = {
   locales: ['ja'],
   defaultLocale: 'ja',
-  pages: {
-    '*': ['common'],
-    '/': ['home'],
-    '/login': ['login'],
-  },
   loadLocaleFrom: (lang, ns) =>
     // You can use a dynamic import, fetch, whatever. You should
     // return a Promise with the JSON file.
     import(`/src/locales/${lang}/${ns}.json`).then((m) => m.default),
   logBuild: false,
+  pages: {
+    '*': ['common'],
+    '/': ['home'],
+    '/login': ['login'],
+  },
 };
